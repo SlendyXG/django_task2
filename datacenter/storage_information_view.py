58d2041021101cdef47078a897e7cdc5dfb97aac
SlendyXG <105538051+SlendyXG@users.noreply.github.com>
2026-07-30 01:53:08 +0800

    Initial commit

---
+++ datacenter/storage_information_view.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from django.shortcuts import render
+from django.utils.timezone import localtime
+
+
+from .models import Visit
+
+from main import get_duration, format_duration
+
+
+def storage_information_view(request):
+    active_visits = Visit.objects.filter(leaved_at__isnull=True)
+
+    non_closed_visits = []
+    for visit in active_visits:
+        duration_seconds = get_duration(visit)
+
+        non_closed_visits.append({
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': localtime(visit.entered_at).strftime('%d-%m-%Y %H:%M'),
+            'duration': format_duration(duration_seconds),
+        })
+
+    context = {
+        'non_closed_visits': non_closed_visits,
+    }
+    return render(request, 'storage_information.html', context)
